220a94f81560229968b3870900cfa116d91ecd1e
Tamara <t.talanceva@yandex.ru>
2024-12-08 22:39:46 +0700

    added DemoTestFirst.java file
    updated build.gradle

---
+++ src/test/java/DemoTestFirst.java
@@ -0,0 +1,66 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+
+
+public class DemoTestFirst {
+
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browser="firefox";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        //Configuration.holdBrowserOpen = true;
+        Configuration.pageLoadStrategy = "eager";
+
+    }
+
+
+    @Test
+    void firstTest() {
+        System.out.println("###      firstTest()");
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#firstName").setValue("Ivan");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("pochta@gmail.com");
+        $("[for='gender-radio-1']").click();
+        $("#userNumber").setValue("9998887766");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__year-select").selectOption("1980");
+        $$(".react-datepicker__day").findBy(text("11")).click();
+        $("#subjectsInput").setValue("any value");
+        $("[for='hobbies-checkbox-2']").click();
+        $("[for='hobbies-checkbox-1']").click();
+        $("#uploadPicture").uploadFromClasspath("капча.jpg");
+        $("#currentAddress").setValue("Moscow, Kremlin");
+        $(".css-tlfecz-indicatorContainer").click();
+        $("#react-select-3-option-2").click();
+        $(".css-tlfecz-indicatorContainer").click();
+        $("#react-select-4-option-0").click();
+        $("#submit").click();
+
+        //проверка значений
+        $(".table-responsive").shouldHave(text("Ivan"));
+        $(".table-responsive").shouldHave(text("Ivanov"));
+        $(".table-responsive").shouldHave(text("pochta@gmail.com"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("9998887766"));
+        $(".table-responsive").shouldHave(text("11 February,1980"));
+        $(".table-responsive").shouldHave(text("any value"));
+        $(".table-responsive").shouldHave(text("Reading, Sports"));
+        $(".table-responsive").shouldHave(text("капча.jpg"));
+        $(".table-responsive").shouldHave(text("Moscow, Kremlin"));
+        $(".table-responsive").shouldHave(text("Haryana Karnal"));
+
+    }
+}
